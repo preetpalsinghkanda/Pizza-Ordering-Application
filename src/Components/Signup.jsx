24fd331c3526a9d3914a41164c3fc46adce6fde8
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-04-09 02:18:44 +0530

    added signup page

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./Signup.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faCircle, faUser} from "@fortawesome/free-solid-svg-icons";
+import {
+  faEye,
+  faCircle,
+  faUser,
+  faEnvelope,
+  faPhone,
+  faLock,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Signup() {
   return (
@@ -12,43 +19,61 @@ export default function Signup() {
           <p>Join us and start ordering</p>
         </div>
 
-        <div>
+        <div className="input-container">
           <div className="input-box">
             <label htmlFor="name">Full Name</label>
-            <div><FontAwesomeIcon icon={faUser}  /><input type="text" name="" id="name" placeholder="Preetpal Singh Kanda" /></div>
+            <div className="input-icon-box">
+              <FontAwesomeIcon icon={faUser} />
+              <input
+                type="text"
+                name=""
+                id="name"
+                placeholder="Preetpal Singh Kanda"
+              />
+            </div>
           </div>
+
           <div className="input-box">
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" />
+            <div className="input-icon-box">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <input type="text" id="email" placeholder="preet@gmail.com" />
+            </div>
           </div>
+
           <div className="input-box">
             <label htmlFor="phone-no">Phone Number</label>
-            <input type="phone" id="phone-no" />
+            <div className="input-icon-box">
+              <FontAwesomeIcon icon={faPhone} />
+              <input type="phone" id="phone-no" placeholder="+1 999 999 9999" />
+            </div>
           </div>
 
           <div className="input-pass-container">
             <div className="input-box ">
               <label htmlFor="pass">Password</label>
-              <div className="pass-input">
+              <div className="pass-input input-icon-box">
+                <FontAwesomeIcon icon={faLock} />
                 <input type="password" id="pass" />
 
-                <FontAwesomeIcon icon={faEye} />
+                <FontAwesomeIcon icon={faEye} className="eye-icon" />
               </div>
             </div>
 
             <div className="input-box ">
               <label htmlFor="pass-confirm">Confirm Password</label>
-              <div className="pass-input">
+              <div className="pass-input input-icon-box">
+                <FontAwesomeIcon icon={faLock} />
                 <input type="password" id="pass-confirm" />
 
-                <FontAwesomeIcon icon={faEye} />
+                <FontAwesomeIcon icon={faEye} className="eye-icon" />
               </div>
             </div>
           </div>
 
-          <div>
+          <div className="input-box">
             <label htmlFor="gender">Gender</label>
-            <div>
+            <div className="gender-boxes">
               <button>Male</button>
               <button>Female</button>
               <button>Other</button>
