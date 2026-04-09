7f5e0cffee7f2b5d06ac80d065fd58ece7696b89
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-04-10 00:42:09 +0530

    added Build Your Pizza

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -9,6 +9,10 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 import pizzaApp from "../Context/Context";
+import pizzaSmall from "../../public/sixslicepizza.png";
+import pizzaMedium from "../../public/eightslicepizza.png";
+import pizzaLarge from "../../public/tenslicepizza.png";
+import pizzaExtraLarge from "../../public/twelveslicepizza.png";
 
 export default function Menu() {
   const { activeSlider } = useContext(pizzaApp);
@@ -67,24 +71,70 @@ export default function Menu() {
         </div>
       </div>
 
-
       <div className="menu-main">
-
+        
         <div className="menu-size">
-          
+          <h3>Select Your Size</h3>
+          <div className="menu-option-container">
+            <div className="menu-option">
+              <div className="menu-option-box">
+                <div>
+                  <span>8"</span>
+                  <p>Small</p>
+                </div>
+                <span className="pizza-price">$8.99</span>
+              </div>
+              <img src={pizzaSmall} alt="" />
+            </div>
 
+            <div className="menu-option">
+              <div className="menu-option-box">
+                <div>
+                  <span>12"</span>
+                  <p>Medium</p>
+                </div>
+                <span className="pizza-price">$12.99</span>
+              </div>
+              <img src={pizzaMedium} alt="" />
+            </div>
+
+            <div className="menu-option">
+              <div className="menu-option-box">
+                <div>
+                  <span>14"</span>
+                  <p>Large</p>
+                </div>
+                <span className="pizza-price">$15.99</span>
+              </div>
+              <img src={pizzaLarge} alt="" />
+            </div>
+
+            <div className="menu-option">
+              <div className="menu-option-box">
+                <div>
+                  <span>16"</span>
+                  <p>Extra Large</p>
+                </div>
+                <span className="pizza-price">$18.99</span>
+              </div>
+              <img src={pizzaExtraLarge} alt="" />
+            </div>
+          </div>
         </div>
 
         <div className="order-preview">
           <h5>Order Preview</h5>
           <ul className="order-list">
-            <li >
+            <li>
               <p>Base (Small)</p>
               <span>$8.99</span>
             </li>
             <hr className="order-preview-hr" />
           </ul>
-         <div className="total-box"><span className="total-name">Total</span><span className="total-bill">$12.99</span></div>
+          <div className="total-box">
+            <span className="total-name">Total</span>
+            <span className="total-bill">$12.99</span>
+          </div>
         </div>
       </div>
     </section>
